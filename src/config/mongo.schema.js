27f56ae2bbde3f80d7b113307d0c3273ba49df12
Alex Ferrer <ferrerale@media-saturn.com>
2019-09-06 13:14:45 +0200

    mongoose

--- src/config/mongo.schema.js
+++ src/config/mongo.schema.js
@@ -8,5 +8,6 @@ const expenseSchema = new mongoose.Schema({
 
 const depositSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    amount: { type: Number, required: true }
+    amount: { type: Number, required: true },
+    recurrence: ['daily', 'monthly', 'yearly']
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 
 // Routes
-import authRouter from './routes/auth'
-import expensesRoutes from './routes/expenses'
+import authRouter from './routes/auth.routes'
+import expensesRoutes from './routes/expenses.routes'
+import mongoose from 'mongoose';
 
 // Create Express Server
 var app = express();
@@ -10,6 +11,15 @@ var app = express();
 // Register routes
 app.use('/auth', authRouter);
 app.use('/expenses', expensesRoutes);
-  
+
+mongoose.connect('mongodb://127.0.0.1:27017/database', { useNewUrlParser: true })
+    .then(() => {
+        console.log('Database connection successful')
+    })
+    .catch(err => {
+        console.error('Database connection error')
+    })
+
+
 // Listen on port 3000
 app.listen(3000);
